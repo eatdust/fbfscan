32adf7fd7189c4d07f234c2648d5dce882a5a67f
chris <chris@ringeval.com>
2020-10-30 17:59:43 +0100

    add config.ini to package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fbfscan",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
@@ -17,7 +17,7 @@ setup(
     zip_safe=False,
     scripts=[],
     install_requires=['wipistepper','gphoto2','curses-menu','curtsies'],
-    package_data={'': ['*.txt', '*.rst'],
+    package_data={'': ['*.txt', '*.rst','config.ini'],
     },
 
     # metadata to display on PyPI
